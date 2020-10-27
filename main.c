8d292045cdc59c9665c278e037927cd3f4894470
Brendan Faulkes <breadmanfox@gmail.com>
2020-10-27 10:26:48 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main;
 {
     int first, second, third, fourth;
     printf("Enter four integers separated by a space:");
@@ -12,16 +12,18 @@ int main(void)
         printf("\n2nd number is lovely");
     else if((third>second) && (third>first) && (third>fourth))
         printf("\n3rd number is the business");
-    else if((4th>1st) && (4th>2nd) && (4th>3rd))
+    else if((fourth>second) && (fourth>third) && (fourth>first))
         printf("4th number is huge");
 
-    if((1st<2nd) && (1st<3rd) && (1st<4th))
+    if((first<second) && (first<third) && (first<fourth))
         printf("\n1st number is tiniest");
-    else if((2nd<1st) && (2nd<3rd) && (2nd<4th))
+    else if((second<first) && (second<third) && (second<fourth))
         printf("\n2nd number is the worst one");
-    else if((3rd<2nd) && (3rd<1st) && (3rd<4th))
+    else if((third<second) && (third<first) && (third<fourth))
         printf("\n3rd number is terrible");
-    else if((4th<2nd) && (4th<3rd) && (4th<1st))
+    else if((fourth<second) && (fourth<third) && (fourth<first))
         printf("\n4th number is RUBBISH");
+
+    printf("\n")
     return 0;
 }
